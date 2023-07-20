a9d6c5d2f86bd01b1f1d55f230ca85251175cf18
sabrina <sabrinizem@gmail.com>
2023-07-20 21:33:46 +0200

    Initial commit

---
+++ app/src/main/java/fr/doranco/projetfinal/fragment/HomeFragment.java
@@ -0,0 +1,39 @@
+package fr.doranco.projetfinal.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.doranco.projetfinal.R;
+import fr.doranco.projetfinal.databinding.FragmentHomeBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+ private FragmentHomeBinding binding;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding=FragmentHomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+
+    }
+}
