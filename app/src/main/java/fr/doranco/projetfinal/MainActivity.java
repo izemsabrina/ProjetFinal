e2dbd50bdd7c8de4491b755a251848a088c18553
sabrina <sabrinizem@gmail.com>
2023-07-26 15:06:37 +0200

    initianl navigation

--- app/src/main/java/fr/doranco/projetfinal/MainActivity.java
+++ app/src/main/java/fr/doranco/projetfinal/MainActivity.java
@@ -1,14 +1,57 @@
 package fr.doranco.projetfinal;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import fr.doranco.projetfinal.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+  private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolBar);
+        //configuration de connection de navigation avec le navigation view
+
+        NavHostFragment navHostFragment=
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+
+        AppBarConfiguration appBarConfiguration =
+        new AppBarConfiguration.Builder(R.id.homeFragment, R.id.paysListFragment)
+                .setOpenableLayout(binding.drawerLayout)
+                                .build();
+
+        NavigationUI.setupWithNavController (
+          binding.navView,
+                navController
+               // appBarConfiguration
+        );
+        NavigationUI.setupActionBarWithNavController(
+               this,navController,appBarConfiguration
+        );
+        //Ajouter le toggele pour ouvrir et fermer la navigation
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                binding.toolBar,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
---
+++ app/src/main/java/fr/doranco/projetfinal/fragment/PaysListFragment.java
@@ -0,0 +1,29 @@
+package fr.doranco.projetfinal.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.doranco.projetfinal.R;
+
+
+public class PaysListFragment extends Fragment {
+
+
+    public PaysListFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pays_list, container, false);
+    }
+}
