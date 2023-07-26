c79c6e774c02aa749ba0eec50e2c6c9ed2e02a90
sabrina <sabrinizem@gmail.com>
2023-07-26 17:10:10 +0200

    initianl afficher la liste de pays

--- app/src/main/java/fr/doranco/projetfinal/adapter/PaysRecycleViewAdapter.java
+++ app/src/main/java/fr/doranco/projetfinal/adapter/PaysRecycleViewAdapter.java
@@ -1,15 +1,23 @@
 package fr.doranco.projetfinal.adapter;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import fr.doranco.projetfinal.R;
+
 public class PaysRecycleViewAdapter {
     protected  class PaysViewHolder extends RecyclerView.ViewHolder{
 
+         public ImageView imageView;
+         public TextView textView;
         public PaysViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imageViewRecyclerViewItemPays);
+            textView = itemView.findViewById(R.id.textViewRecyclerViewItemPays);
         }
     }
 }
